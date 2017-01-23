e0ccc5e2891fd1279a2365626cc32572a2eae32d
paola <paolatorobejar@gmail.com>
2017-01-23 17:00:16 -0500

    salida de elementos ok

--- js/main.js
+++ js/main.js
@@ -1,32 +1,54 @@
 
-var boton=document.getElementById("boton");
-var input=document.getElementById("mensaje");
 var lista=document.getElementById("lista");
+var input=document.getElementById("mensaje");
+var boton=document.getElementById("boton");
+
 
 boton.addEventListener("click", onButtonClick);
 
 function onButtonClick()
 {
-	agregarMensaje();
+	agregarTarea();
 }
-function agregarMensaje()
+
+function agregarTarea()
 {
-	var mensaje=input.value;
-	var item =document.createElement("li");
-	var span=document.createElement("span");
-
-	item.innerHTML =mensaje;
+	var tarea=input.value;
+	var vacio=input.value;
+	var	item=document.createElement("li");
+	var	enlace=document.createElement("a");
+		contenido=document.createTextNode(tarea); 
 	
-	input.value="";
-	input.focus();
 
-	lista.appendChild(item);
+		input.value="";
+		input.focus();
 
-	lista.addEventListener("click", onItemClick);
-	//lista.innerHTML += "<li>" + mensaje + "</li>";
+		lista.appendChild(item);
+
+		lista.addEventListener("click", onItemClick);
+		//lista.innerHTML += "<li>" + mensaje + "</li>";
+
+			
+	
+	if(vacio===""){
+		input.setAttribute("placeholder","Agrega tarea valida");
+		input.style.border ="solid 3px red";
+		return false;
+		}else{
+			input.setAttribute("placeholder","Añade tu tarea nueva");
+			input.style.border="solid 3px green";
+		}
+
+	enlace.appendChild(contenido);
+	enlace.setAttribute("href","#")
+	item.appendChild(enlace);
+
 }
+
 function onItemClick(evt)
 {
 	console.log(evt.target);
 	evt.target.parentNode.removeChild(evt.target);
 }
+
+
